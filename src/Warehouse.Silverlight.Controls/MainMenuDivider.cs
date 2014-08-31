37db28507b82910d6f942e972fea470fd6d45642
Yauheni Khvaliuk <hvaluk@gmail.com>
2014-08-31 22:02:50 +0200

    main menu dividers added

---
+++ src/Warehouse.Silverlight.Controls/MainMenuDivider.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Warehouse.Silverlight.Controls
+{
+    public class MainMenuDivider : Control
+    {
+        public MainMenuDivider()
+        {
+            DefaultStyleKey = typeof (MainMenuDivider);
+        }
+    }
+}
