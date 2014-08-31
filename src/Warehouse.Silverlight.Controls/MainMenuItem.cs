abddcda1c7fe705bc59a7a1bdd0094f1166ed410
Yauheni Khvaliuk <hvaluk@gmail.com>
2014-08-31 21:47:32 +0200

    main menu started

---
+++ src/Warehouse.Silverlight.Controls/MainMenuItem.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Warehouse.Silverlight.Controls
+{
+    public class MainMenuItem : RadioButton
+    {
+        public MainMenuItem()
+        {
+            DefaultStyleKey = typeof(MainMenuItem);
+        }
+    }
+}
