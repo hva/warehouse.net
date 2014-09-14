63a1ab3345cd4e4a60e92cbb04b742911435c949
Yauheni Khvaliuk <hvaluk@gmail.com>
2014-09-14 21:31:45 +0200

    top menu moved to navigation assembly

--- src/Warehouse.Silverlight.NavigationModule/NavigationModule.cs
+++ src/Warehouse.Silverlight.NavigationModule/NavigationModule.cs
@@ -1,11 +1,21 @@
 ﻿using Microsoft.Practices.Prism.Modularity;
+using Microsoft.Practices.Prism.Regions;
+using Microsoft.Practices.Unity;
+using Warehouse.Silverlight.NavigationModule.Views;
 
 namespace Warehouse.Silverlight.NavigationModule
 {
     public class NavigationModule : IModule
     {
+        [Dependency]
+        public IUnityContainer Container { get; set; }
+
+        [Dependency]
+        public IRegionManager RegionManager { get; set; }
+
         public void Initialize()
         {
+            RegionManager.RegisterViewWithRegion("NavigationRegion", () => Container.Resolve<TopMenu>());
         }
     }
 }
