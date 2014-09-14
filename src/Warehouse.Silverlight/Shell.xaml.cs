ef93209c3583a421693e2da1c5a26d36a3d46f64
Yauheni Khvaliuk <hvaluk@gmail.com>
2014-09-14 22:49:33 +0200

    version label added to shell

--- src/Warehouse.Silverlight/Shell.xaml.cs
+++ src/Warehouse.Silverlight/Shell.xaml.cs
@@ -1,10 +1,17 @@
-﻿namespace Warehouse.Silverlight
+﻿using System.Reflection;
+
+namespace Warehouse.Silverlight
 {
     public partial class Shell
     {
         public Shell()
         {
             InitializeComponent();
+
+            var assembly = Assembly.GetExecutingAssembly();
+            var assemblyName = new AssemblyName(assembly.FullName);
+            var version = assemblyName.Version.ToString().TrimEnd(".0".ToCharArray());
+            VersionTextBlock.Text = string.Concat("v.", version);
         }
     }
 }
