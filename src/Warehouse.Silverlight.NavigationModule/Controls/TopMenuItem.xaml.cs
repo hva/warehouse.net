6eadf2c60264a037c94efb2f85f87e284da147e7
Yauheni Khvaliuk <hvaluk@gmail.com>
2014-09-14 22:07:47 +0200

    controls assembly removed

---
+++ src/Warehouse.Silverlight.NavigationModule/Controls/TopMenuItem.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Warehouse.Silverlight.NavigationModule.Controls
+{
+    public partial class TopMenuItem : RadioButton
+    {
+        public TopMenuItem()
+        {
+            InitializeComponent();
+        }
+    }
+}
