20f5879dff0372286d09deb8b6005b0b9645dcf3
Yauheni Khvaliuk <hvaluk@gmail.com>
2014-09-25 21:47:15 +0200

    entity framework removed

--- src/Warehouse.Server/Controllers/TaxonomyController.cs
+++ src/Warehouse.Server/Controllers/TaxonomyController.cs
@@ -1,7 +1,5 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using System.Web.Http;
-using Warehouse.Server.Data;
 using Warehouse.Server.Models;
 
 namespace Warehouse.Server.Controllers
@@ -10,8 +8,7 @@ namespace Warehouse.Server.Controllers
     {
         public IEnumerable<Taxonomy> Get()
         {
-            var context = new SklContext();
-            return context.Taxonomy.ToArray();
+            return new Taxonomy[] {};
         }
     }
 }
