db492c1f25ce6a2d0e7f1ce567b49ab431d6ea58
Yauheni Khvaliuk <hvaluk@gmail.com>
2014-09-28 20:35:57 +0200

    silverlight - main view grid columns

--- src/Warehouse.Silverlight.Models/Product.cs
+++ src/Warehouse.Silverlight.Models/Product.cs
@@ -1,9 +1,33 @@
-﻿namespace Warehouse.Silverlight.Models
+﻿using System.Globalization;
+using System.Linq;
+
+namespace Warehouse.Silverlight.Models
 {
     public class Product
     {
         public string Id { get; set; }
         public string Name { get; set; }
         public string Size { get; set; }
+        public double K { get; set; }
+        public int PriceOpt { get; set; }
+        public int PriceRozn { get; set; }
+        public double Weight { get; set; }
+        public int Count { get; set; }
+        public double[] Nd { get; set; }
+        public double Length { get; set; }
+        public string Internal { get; set; }
+        public long PriceIcome { get; set; }
+
+        public string NdTotal
+        {
+            get
+            {
+                if (Nd == null || Nd.Length == 0)
+                {
+                    return null;
+                }
+                return Nd.Sum().ToString(CultureInfo.InvariantCulture);
+            }
+        }
     }
 }
