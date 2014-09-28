a15779ced0e5045caf0f2350b3f223db94d6e56b
Yauheni Khvaliuk <hvaluk@gmail.com>
2014-09-28 09:18:31 +0200

    taxonomy removed

---
+++ src/Warehouse.Server.Models/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace Warehouse.Server.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+    }
+}
---
+++ src/Warehouse.Server/Controllers/ProductsController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using Warehouse.Server.Models;
+
+namespace Warehouse.Server.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        public IEnumerable<Product> Get()
+        {
+            return new []
+            {
+                new Product { Id = 123122 },
+                new Product { Id = 245345 },
+            };
+        }
+    }
+}
