4838da9fa7f9cca46f51d8abdfc08a9ba4302e20
Yauheni Khvaliuk <hvaluk@gmail.com>
2014-10-12 18:33:48 +0200

    core installed

---
+++ src/Warehouse.Server/Hubs/ProductsHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace Warehouse.Server.Hubs
+{
+    public class ProductsHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+    }
+}
---
+++ src/Warehouse.Server/Hubs/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Owin;
+using Owin;
+using Warehouse.Server.Hubs;
+
+[assembly: OwinStartup(typeof(Startup))]
+
+namespace Warehouse.Server.Hubs
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
