abf63164f4ce8aa8f2a3ee2818bd760af17102a2
Yauheni Khvaliuk <hvaluk@gmail.com>
2014-10-26 19:51:43 +0100

    silverlight - product editor - close by escape key press

---
+++ src/Warehouse.Silverlight.MainModule/Behaviors/ChildWindowBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Interactivity;
+
+namespace Warehouse.Silverlight.MainModule.Behaviors
+{
+    public class ChildWindowBehavior : Behavior<ChildWindow>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.KeyUp += OnKeyUp;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            AssociatedObject.KeyUp -= OnKeyUp;
+        }
+
+        private void OnKeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+            {
+                AssociatedObject.Close();
+            }
+        }
+    }
+}
