c1f5b16fc195787d97a3e78c74b94d8ac851c6ab
Yauheni Khvaliuk <hvaluk@gmail.com>
2014-11-02 18:51:30 +0100

    html - login page added

---
+++ src/Warehouse.Server/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+
+namespace Warehouse.Server.Controllers
+{
+    public class UserController : Controller
+    {
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string login, string password)
+        {
+            return View();
+        }
+    }
+}
