a7801cff42268407ab2179cdb4101290ddc55e16
Yauheni Khvaliuk <hvaluk@gmail.com>
2014-11-02 19:44:00 +0100

    error message added

--- src/Warehouse.Server/Controllers/UserController.cs
+++ src/Warehouse.Server/Controllers/UserController.cs
@@ -1,18 +1,38 @@
-﻿using System.Web.Mvc;
+﻿using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity.Owin;
+using Warehouse.Server.Identity;
 
 namespace Warehouse.Server.Controllers
 {
     public class UserController : Controller
     {
+        private const string LoginError = "The password you entered is incorrect. Please try again.";
+
         [HttpGet]
-        public ActionResult Login()
+        public ActionResult Login(string returnUrl)
         {
+            ViewBag.ReturnUrl = returnUrl;
             return View();
         }
 
         [HttpPost]
-        public ActionResult Login(string login, string password)
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Login(string login, string password, string returnUrl)
         {
+            ViewBag.ReturnUrl = returnUrl;
+            ViewBag.Login = login;
+
+            var userManager = HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            var user = await userManager.FindByNameAsync(login);
+
+            if (user == null)
+            {
+                ModelState.AddModelError(string.Empty, LoginError);
+                return View();
+            }
+
             return View();
         }
     }
