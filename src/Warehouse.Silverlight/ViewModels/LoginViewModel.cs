6c0bfffce4b1578ac8653aaf23369887e7059749
Yauheni Khvaliuk <hvaluk@gmail.com>
2014-11-09 21:06:40 +0100

    login page UI

--- src/Warehouse.Silverlight/ViewModels/LoginViewModel.cs
+++ src/Warehouse.Silverlight/ViewModels/LoginViewModel.cs
@@ -1,16 +1,21 @@
 ﻿using System.Windows.Input;
 using Microsoft.Practices.Prism.Commands;
+using Microsoft.Practices.Prism.ViewModel;
 using Warehouse.Silverlight.DataService.Auth;
+using Warehouse.Silverlight.Navigation;
 
 namespace Warehouse.Silverlight.ViewModels
 {
-    public class LoginViewModel
+    public class LoginViewModel : NotificationObject
     {
+        private int messageOpacity;
         private readonly IAuthService authService;
+        private readonly INavigationService navigationService;
 
-        public LoginViewModel(IAuthService authService)
+        public LoginViewModel(IAuthService authService, INavigationService navigationService)
         {
             this.authService = authService;
+            this.navigationService = navigationService;
 
             LoginCommand = new DelegateCommand(DoLogin);
         }
@@ -19,12 +24,23 @@ namespace Warehouse.Silverlight.ViewModels
         public string Password { get; set; }
         public ICommand LoginCommand { get; set; }
 
+        public int MessageOpacity
+        {
+            get { return messageOpacity; }
+            set { messageOpacity = value; RaisePropertyChanged(() => MessageOpacity); }
+        }
+
         private async void DoLogin()
         {
             var task = await authService.Login(Login, Password);
             if (task.Succeed)
             {
-                
+                MessageOpacity = 0;
+                navigationService.OpenLandingPage();
+            }
+            else
+            {
+                MessageOpacity = 1;
             }
         }
     }
