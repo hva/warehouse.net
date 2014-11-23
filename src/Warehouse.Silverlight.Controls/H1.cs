a9ece453e54d838f9f317d03e2e737ec4238c33d
Yauheni Khvaliuk <hvaluk@gmail.com>
2014-11-23 22:05:20 +0100

    silverlight - h1 control added

---
+++ src/Warehouse.Silverlight.Controls/H1.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Warehouse.Silverlight.Controls
+{
+    public class H1 : ContentControl
+    {
+        public H1()
+        {
+            DefaultStyleKey = typeof (H1);
+        }
+
+        #region Title
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register("Title", typeof(string), typeof(H1), new PropertyMetadata(null));
+
+        #endregion
+    }
+}
