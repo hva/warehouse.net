8d67afb2e17d5afb4f05d274cfb032f27f6e3cbf
Yauheni Khvaliuk <hvaluk@gmail.com>
2014-11-24 13:40:23 +0100

    version changed to 0.0.6

--- src/Warehouse.Silverlight.NavigationModule/Views/LoggedInView.xaml.cs
+++ src/Warehouse.Silverlight.NavigationModule/Views/LoggedInView.xaml.cs
@@ -1,17 +1,11 @@
-﻿using System.Reflection;
-
-namespace Warehouse.Silverlight.NavigationModule.Views
+﻿namespace Warehouse.Silverlight.NavigationModule.Views
 {
     public partial class LoggedInView
     {
         public LoggedInView()
         {
             InitializeComponent();
-
-            var assembly = Assembly.GetExecutingAssembly();
-            var assemblyName = new AssemblyName(assembly.FullName);
-            var version = assemblyName.Version.ToString().TrimEnd(".0".ToCharArray());
-            VersionTextBlock.Text = string.Concat("client v.", version);
+            VersionTextBlock.Text = "version 0.0.6";
         }
     }
 }
