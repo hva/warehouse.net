4a101b5cb9721287ea34d8e1beb12fff0940379e
Yauheni Khvaliuk <hvaluk@gmail.com>
2014-12-17 17:41:57 +0100

    utils - user roles support

--- src/Warehouse.Utils.CreateUser/Program.cs
+++ src/Warehouse.Utils.CreateUser/Program.cs
@@ -12,17 +12,18 @@ namespace Warehouse.Utils.CreateUser
     {
         static void Main(string[] args)
         {
-            if (args == null || args.Length < 2)
+            if (args == null || args.Length < 3)
             {
                 string codeBase = Assembly.GetExecutingAssembly().CodeBase;
                 string exeName = Path.GetFileName(codeBase);
 
-                Console.WriteLine("usage: {0} <username> <password>", exeName);
+                Console.WriteLine("usage: {0} <username> <password> <role>", exeName);
                 return;
             }
 
             var username = args[0];
             var password = args[1];
+            var role = args[2];
 
             var mongoContext = new MongoContext();
             var identityContext = new ApplicationIdentityContext(mongoContext);
@@ -33,7 +34,8 @@ namespace Warehouse.Utils.CreateUser
             var result = manager.Create(user, password);
             if (result.Succeeded)
             {
-                var roleResult = manager.AddUserToRolesAsync(user.Id, new[] {"admin"});
+                var roles = new[] { role };
+                var roleResult = manager.AddUserToRolesAsync(user.Id, roles);
                 roleResult.Wait();
 
                 Console.WriteLine("user created!");
