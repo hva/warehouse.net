3d0e51d63f8b77baad27c79fab787937d4a7ea55
Yauheni Khvaliuk <hvaluk@gmail.com>
2015-01-19 01:43:00 +0100

    products list n/d tooltip #3 fix

---
+++ src/Warehouse.Silverlight.Controls/Converters/NdValueConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Warehouse.Silverlight.Controls.Converters
+{
+    public class NdValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double[] parts = value as double[];
+            if (parts != null)
+            {
+                return string.Join("\n", parts);
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Warehouse.Silverlight.Controls/Converters/NdVisibilityConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Warehouse.Silverlight.Controls.Converters
+{
+    public class NdVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double[] parts = value as double[];
+            if (parts != null && parts.Length > 1)
+            {
+                return Visibility.Visible;
+            }
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
