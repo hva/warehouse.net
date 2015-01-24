729c0d377431622ba44c6035d51ea18189f6b6f6
Yauheni Khvaliuk <hvaluk@gmail.com>
2015-01-25 00:42:29 +0100

    silverlight - main - data grid row background converter

---
+++ src/Warehouse.Silverlight.Controls/Behaviors/DataGridRowBackgroundBehavior.cs
@@ -0,0 +1,44 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Interactivity;
+using System.Windows.Media;
+
+namespace Warehouse.Silverlight.Controls.Behaviors
+{
+    public class DataGridRowBackgroundBehavior : Behavior<DataGrid>
+    {
+        #region Converter
+
+        public IValueConverter Converter
+        {
+            get { return (IValueConverter)GetValue(ConverterProperty); }
+            set { SetValue(ConverterProperty, value); }
+        }
+
+        public static readonly DependencyProperty ConverterProperty =
+            DependencyProperty.Register("Converter", typeof(IValueConverter), typeof(DataGridRowBackgroundBehavior), new PropertyMetadata(null));
+
+        #endregion
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.LoadingRow += LoadingRow;
+            AssociatedObject.Unloaded += Unloaded;
+        }
+
+        private void Unloaded(object sender, RoutedEventArgs e)
+        {
+            AssociatedObject.LoadingRow -= LoadingRow;
+        }
+
+        private void LoadingRow(object sender, DataGridRowEventArgs e)
+        {
+            if (Converter == null) return;
+
+            e.Row.Background = Converter.Convert(e.Row.DataContext, typeof(Brush), null, CultureInfo.InvariantCulture) as Brush;
+        }
+    }
+}
---
+++ src/Warehouse.Silverlight.MainModule/Converters/ProductFirmaToBrushConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Warehouse.Silverlight.MainModule.Converters
+{
+    public class ProductFirmaToBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return new SolidColorBrush(Colors.Red);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
