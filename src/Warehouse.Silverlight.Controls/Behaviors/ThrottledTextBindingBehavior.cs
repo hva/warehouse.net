165a7235b6d6b06be0a87d99151d5b22bb799b97
Yauheni Khvaliuk <hvaluk@gmail.com>
2015-02-14 18:35:54 +0100

    silverlight - product editor - ThrottledTextBindingBehavior added

---
+++ src/Warehouse.Silverlight.Controls/Behaviors/ThrottledTextBindingBehavior.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Interactivity;
+using System.Windows.Threading;
+
+namespace Warehouse.Silverlight.Controls.Behaviors
+{
+    public class ThrottledTextBindingBehavior : Behavior<TextBox>
+    {
+        private BindingExpression expression;
+        private DispatcherTimer timer = new DispatcherTimer { Interval = TimeSpan.FromMilliseconds(400) };
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+
+            expression = AssociatedObject.GetBindingExpression(TextBox.TextProperty);
+            AssociatedObject.TextChanged += OnTextChanged;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            AssociatedObject.TextChanged += OnTextChanged;
+        }
+
+        private void OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (timer.IsEnabled)
+            {
+                timer.Stop();
+                timer.Tick -= Tick;
+            }
+            timer.Tick += Tick;
+            timer.Start();
+        }
+
+        private void Tick(object sender, EventArgs e)
+        {
+            timer.Stop();
+            timer.Tick -= Tick;
+
+            if (expression != null)
+            {
+                expression.UpdateSource();
+            }
+        }
+    }
+}
---
+++ src/Warehouse.Silverlight.MainModule/Converters/ZeroToStringConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Warehouse.Silverlight.MainModule.Converters
+{
+    public class ZeroToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // from code to UI
+            if (string.Equals(value, "0"))
+            {
+                return string.Empty;
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // from UI to code
+            if (string.Equals(value, string.Empty))
+            {
+                return "0";
+            }
+            return value;
+        }
+    }
+}
