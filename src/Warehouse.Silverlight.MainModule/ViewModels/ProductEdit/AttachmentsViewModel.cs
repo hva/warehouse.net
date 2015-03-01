0f7b87e833d7fc187daf6142fbd2c9b03d83850b
Yauheni Khvaliuk <hvaluk@gmail.com>
2015-03-02 00:47:53 +0100

    certificates - file open dialog

--- src/Warehouse.Silverlight.MainModule/ViewModels/ProductEdit/AttachmentsViewModel.cs
+++ src/Warehouse.Silverlight.MainModule/ViewModels/ProductEdit/AttachmentsViewModel.cs
@@ -1,9 +1,39 @@
-﻿namespace Warehouse.Silverlight.MainModule.ViewModels.ProductEdit
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+using Microsoft.Practices.Prism.Commands;
+
+namespace Warehouse.Silverlight.MainModule.ViewModels.ProductEdit
 {
     public class AttachmentsViewModel
     {
+        public AttachmentsViewModel()
+        {
+            BrowseCommand = new DelegateCommand(Browse);
+        }
+
         public void Init(string productId)
         {
+        }
+
+        public ICommand BrowseCommand { get; private set; }
+
+        private void Browse()
+        {
+            var dlg = new OpenFileDialog
+            {
+                Multiselect = false,
+                Filter = "JPEG Images (*.jpg, *.jpeg)|*.jpg;*.jpeg"
+            };
+
+            if (dlg.ShowDialog() == true)
+            {
+                //UploadFile(dlg.File.Name, dlg.File.OpenRead());
+                //StatusText.Text = dlg.File.Name;
+            }
+            else
+            {
+                //StatusText.Text = "No file selected...";
+            }
         }
     }
 }
