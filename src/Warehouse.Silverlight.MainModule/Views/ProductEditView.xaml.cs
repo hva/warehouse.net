838c9501d92de00c2d381a2a6434f6eff4636278
hva <hvaluk@gmail.com>
2015-03-19 22:30:44 +0100

    certificates - always open first tab

--- src/Warehouse.Silverlight.MainModule/Views/ProductEditView.xaml.cs
+++ src/Warehouse.Silverlight.MainModule/Views/ProductEditView.xaml.cs
@@ -1,15 +1,21 @@
-﻿using System.Threading;
-using System.Windows.Controls;
+﻿using System.ComponentModel;
+using System.Threading;
 using System.Windows.Markup;
 
 namespace Warehouse.Silverlight.MainModule.Views
 {
-    public partial class ProductEditView : ChildWindow
+    public partial class ProductEditView
     {
         public ProductEditView()
         {
             Language = XmlLanguage.GetLanguage(Thread.CurrentThread.CurrentCulture.Name);
             InitializeComponent();
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            base.OnClosing(e);
+            FirstTab.IsSelected = true;
         }
     }
 }
