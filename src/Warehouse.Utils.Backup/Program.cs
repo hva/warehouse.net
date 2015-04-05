8fc05a9a93158b363edeb02c8ece14a7f8f86cca
hva <hvaluk@gmail.com>
2015-04-05 12:09:32 +0200

    backup - file upload to yandex disk

--- src/Warehouse.Utils.Backup/Program.cs
+++ src/Warehouse.Utils.Backup/Program.cs
@@ -1,6 +1,12 @@
 ﻿using System;
 using System.Diagnostics;
+using System.IO;
 using System.IO.Compression;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
 using NLog;
 
 namespace Warehouse.Utils.Backup
@@ -11,10 +17,22 @@ namespace Warehouse.Utils.Backup
 
         static void Main(string[] args)
         {
-            if (TryDump())
-            {
-                TryZip();
-            }
+            if (!TryDump()) return;
+
+            var fileName = string.Format("skill_{0:yyyyMMdd_HHmm}.zip", DateTime.Now);
+            if (!TryZip(fileName)) return;
+
+            var token = LoadToken();
+            if (token == null) return;
+
+            var task = GetUploadLink(fileName, token);
+            task.Wait();
+
+            var link = task.Result;
+            if (link == null) return;
+
+            var task2 = UploadFile(fileName, link);
+            task2.Wait();
         }
 
         private static bool TryDump()
@@ -35,20 +53,17 @@ namespace Warehouse.Utils.Backup
 
             if (string.IsNullOrEmpty(error))
             {
-                logger.Trace("dump ok");
                 return true;
             }
             logger.Error(error);
             return false;
         }
 
-        private static bool TryZip()
+        private static bool TryZip(string fileName)
         {
             try
             {
-                var fileName = string.Format("skill_{0:yyyyMMdd_HHmm}.zip", DateTime.Now);
                 ZipFile.CreateFromDirectory("dump", fileName);
-                logger.Trace("zip ok");
                 return true;
             }
             catch (Exception e)
@@ -56,6 +71,63 @@ namespace Warehouse.Utils.Backup
                 logger.Error(e.Message);
             }
             return false;
+        }
+
+        private static string LoadToken()
+        {
+            try
+            {
+                using (var reader = new StreamReader("token.txt"))
+                {
+                    return reader.ReadLine();
+                }
+            }
+            catch(Exception e)
+            {
+                logger.Error(e.Message);
+            }
+            return null;
+        }
+
+        private async static Task<string> GetUploadLink(string fileName, string token)
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("OAuth", token);
+                client.BaseAddress = new Uri("https://cloud-api.yandex.net:443");
+
+                var path = string.Concat("skill-backup/", fileName);
+                var linkUriString = string.Format("/v1/disk/resources/upload?path={0}", WebUtility.UrlEncode(path));
+
+                var resp = await client.GetAsync(linkUriString);
+                var content = await resp.Content.ReadAsStringAsync();
+                if (resp.StatusCode == HttpStatusCode.OK)
+                {
+                    var json = JObject.Parse(content);
+                    return json["href"].ToString();
+                }
+                logger.Error(content);
+                return null;
+            }
+        }
+
+        private static async Task UploadFile(string fileName, string link)
+        {
+            using (var client = new HttpClient())
+            using (var stream = File.OpenRead(fileName))
+            using (var content = new StreamContent(stream))
+            {
+                var resp = await client.PutAsync(link, content);
+                if (resp.StatusCode == HttpStatusCode.Created)
+                {
+                    logger.Trace("upload succeed");
+                }
+                else
+                {
+                    var errorContent = await resp.Content.ReadAsStringAsync();
+                    logger.Error(errorContent);
+                }
+            }
         }
     }
 }
