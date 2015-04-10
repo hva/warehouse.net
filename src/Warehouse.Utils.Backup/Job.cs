900c7290bc8a38e91e83588a9b0759f7ec1ca593
Yauheni Khvaliuk <hvaluk@gmail.com>
2015-04-09 21:12:02 -0700

    backup - absolute paths used

--- src/Warehouse.Utils.Backup/Job.cs
+++ src/Warehouse.Utils.Backup/Job.cs
@@ -15,40 +15,56 @@ namespace Warehouse.Utils.Backup
     public class Job : CronJob
     {
         private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+        private string appPath;
+        private string zipFile;
+        private string token;
+        private string uploadLink;
 
         public override void Execute()
         {
-            if (!Dump()) return;
-
-            var fileName = string.Format("skill_{0:yyyyMMdd_HHmm}.zip", DateTime.Now);
-            if (!Zip(fileName)) return;
-
-            var token = LoadToken();
-            if (token == null) return;
-
-            var task = GetUploadLink(fileName, token);
-            task.Wait();
-
-            var link = task.Result;
-            if (link == null) return;
-
-            var task2 = UploadFile(fileName, link);
-            task2.Wait();
-            if (!task2.Result) return;
-
-            Cleanup(fileName);
+            try
+            {
+                var task = ExecuteAsync();
+                task.Wait();
+            }
+            catch (Exception e)
+            {
+                logger.Error(e.Message);
+            }
         }
 
-        private static bool Dump()
+        public async Task ExecuteAsync()
+        {
+            PreparePaths();
+            Dump();
+            Zip();
+            LoadToken();
+            await GetUploadLink();
+            await UploadFile();
+            Cleanup();
+
+            logger.Trace("ok");
+        }
+
+        private void PreparePaths()
         {
             var codebase = System.Reflection.Assembly.GetExecutingAssembly().CodeBase;
             var uri = new Uri(codebase, UriKind.Absolute);
-            var path = Path.GetDirectoryName(uri.LocalPath);
+            appPath = Path.GetDirectoryName(uri.LocalPath);
+            if (appPath == null)
+            {
+                throw new NullReferenceException("appPath");
+            }
+            var customerNow = DateTime.UtcNow.AddHours(3);
+            zipFile = $"skill_{customerNow:yyyyMMdd_HHmm}.zip";
+        }
 
+        private void Dump()
+        {
             var info = new ProcessStartInfo
             {
                 FileName = "mongodump",
-                Arguments = "--db skill --out " + path,
+                Arguments = "--db skill --out " + Path.Combine(appPath, "dump"),
                 CreateNoWindow = true,
                 UseShellExecute = false,
                 RedirectStandardError = true,
@@ -59,94 +75,88 @@ namespace Warehouse.Utils.Backup
 
             var error = process.StandardError.ReadToEnd();
 
-            if (string.IsNullOrEmpty(error))
+            if (!string.IsNullOrEmpty(error))
             {
-                return true;
+                throw new Exception(error);
             }
-            logger.Error(error);
-            return false;
         }
 
-        private static bool Zip(string fileName)
+        private void Zip()
         {
-            try
-            {
-                ZipFile.CreateFromDirectory("dump", fileName);
-                return true;
-            }
-            catch (Exception e)
-            {
-                logger.Error(e.Message);
-            }
-            return false;
+            var dumpPath = Path.Combine(appPath, "dump");
+            var zipFullPath = Path.Combine(appPath, zipFile);
+            ZipFile.CreateFromDirectory(dumpPath, zipFullPath);
         }
 
-        private static string LoadToken()
+        private void LoadToken()
         {
-            const string fileName = "token.txt";
+            var fileName = Path.Combine(appPath, "token.txt");
             if (!File.Exists(fileName))
             {
-                logger.Error("{0} not found", fileName);
-                return null;
+                throw new FileNotFoundException("file not found", fileName) ;
             }
-            string token;
             using (var reader = new StreamReader(fileName))
             {
                 token = reader.ReadLine();
             }
             if (string.IsNullOrEmpty(token))
             {
-                logger.Error("token is empty");
-                return null;
+                throw new Exception("token is empty");
             }
-            return token;
         }
 
-        private async static Task<string> GetUploadLink(string fileName, string token)
+        private async Task GetUploadLink()
         {
             using (var client = new HttpClient())
             {
                 client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("OAuth", token);
                 client.BaseAddress = new Uri("https://cloud-api.yandex.net:443");
 
-                var path = string.Concat("skill-backup/", fileName);
-                var linkUriString = string.Format("/v1/disk/resources/upload?path={0}", WebUtility.UrlEncode(path));
+                var path = string.Concat("skill-backup/", zipFile);
+                var linkUriString = $"/v1/disk/resources/upload?path={WebUtility.UrlEncode(path)}";
 
                 var resp = await client.GetAsync(linkUriString);
                 var content = await resp.Content.ReadAsStringAsync();
                 if (resp.StatusCode == HttpStatusCode.OK)
                 {
                     var json = JObject.Parse(content);
-                    return json["href"].ToString();
+                    uploadLink = json["href"].ToString();
                 }
-                logger.Error(content);
-                return null;
+                if (string.IsNullOrEmpty(uploadLink))
+                {
+                    throw new Exception("empty upload link");
+                }
             }
         }
 
-        private static async Task<bool> UploadFile(string fileName, string link)
+        private async Task UploadFile()
         {
+            var zipFullPath = Path.Combine(appPath, zipFile);
             using (var client = new HttpClient())
-            using (var stream = File.OpenRead(fileName))
+            using (var stream = File.OpenRead(zipFullPath))
             using (var content = new StreamContent(stream))
             {
-                var resp = await client.PutAsync(link, content);
-                if (resp.StatusCode == HttpStatusCode.Created)
+                var resp = await client.PutAsync(uploadLink, content);
+                if (resp.StatusCode != HttpStatusCode.Created)
                 {
-                    logger.Trace("upload succeed");
-                    return true;
+                    var errorContent = await resp.Content.ReadAsStringAsync();
+                    throw new Exception(errorContent);
                 }
-                var errorContent = await resp.Content.ReadAsStringAsync();
-                logger.Error(errorContent);
-                return false;
             }
         }
 
-        private static void Cleanup(string fileName)
+        private void Cleanup()
         {
-            if (File.Exists(fileName))
+            var dumpPath = Path.Combine(appPath, "dump");
+            if (Directory.Exists(dumpPath))
             {
-                File.Delete(fileName);
+                Directory.Delete(dumpPath, true);
+            }
+
+            var zipFullPath = Path.Combine(appPath, zipFile);
+            if (File.Exists(zipFullPath))
+            {
+                File.Delete(zipFullPath);
             }
         }
     }
