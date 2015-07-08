5ce2e8dc3f49f7b27f5e22ed4a287618be230de3
Yauheni Khvaliuk <hvaluk@gmail.com>
2015-07-08 18:34:37 +0200

    product details - window behavior added

---
+++ src/Warehouse.Wpf.Controls/Behaviors/WindowBehavior.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Interactivity;
+
+namespace Warehouse.Wpf.Controls.Behaviors
+{
+    public class WindowBehavior : Behavior<Window>
+    {
+        public bool IsWindowOpen
+        {
+            get { return (bool)GetValue(IsWindowOpenProperty); }
+            set { SetValue(IsWindowOpenProperty, value); }
+        }
+
+        public static readonly DependencyProperty IsWindowOpenProperty =
+            DependencyProperty.Register("IsWindowOpen", typeof(bool), typeof(WindowBehavior), new PropertyMetadata(false, OnIsWindowOpenChanged));
+
+        private static void OnIsWindowOpenChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ((WindowBehavior)d).UpdateIsWindowOpen();
+        }
+
+        private void UpdateIsWindowOpen()
+        {
+            // is used only for closing
+            if (!IsWindowOpen)
+            {
+                AssociatedObject.Close();
+            }
+        }
+    }
+}
