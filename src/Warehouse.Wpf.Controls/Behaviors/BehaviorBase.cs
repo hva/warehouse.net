68c9f60de795d6886bda9b081cf6d20e01278d0c
Yauheni Khvaliuk <hvaluk@gmail.com>
2015-07-11 19:39:54 +0200

    desktop - product list - double click behavior

---
+++ src/Warehouse.Wpf.Controls/Behaviors/BehaviorBase.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+using System.Windows.Interactivity;
+
+namespace Warehouse.Wpf.Controls.Behaviors
+{
+    public abstract class BehaviorBase<T> : Behavior<T> where T : FrameworkElement
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.Loaded += OnLoaded;
+            AssociatedObject.Unloaded += OnUnloaded;
+        }
+
+        protected override void OnDetaching()
+        {
+            AssociatedObject.Loaded -= OnLoaded;
+            AssociatedObject.Unloaded -= OnUnloaded;
+            base.OnDetaching();
+        }
+
+        protected virtual void OnLoaded()
+        {
+        }
+
+        protected virtual void OnUnloaded()
+        {
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            OnLoaded();
+        }
+
+        private void OnUnloaded(object sender, RoutedEventArgs e)
+        {
+            OnUnloaded();
+        }
+    }
+}
---
+++ src/Warehouse.Wpf.Controls/Behaviors/ListViewDoubleClickBehavior.cs
@@ -0,0 +1,57 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Warehouse.Wpf.Controls.Behaviors
+{
+    public class ListViewDoubleClickBehavior : BehaviorBase<ListView>
+    {
+        #region Command
+
+        public ICommand Command
+        {
+            get { return (ICommand)GetValue(CommandProperty); }
+            set { SetValue(CommandProperty, value); }
+        }
+
+        public static readonly DependencyProperty CommandProperty = DependencyProperty.Register(
+            "Command",
+            typeof(ICommand),
+            typeof(ListViewDoubleClickBehavior),
+            new PropertyMetadata(null)
+        );
+
+        #endregion
+
+        protected override void OnLoaded()
+        {
+            AssociatedObject.MouseDoubleClick += OnDoubleClick;
+        }
+
+        protected override void OnUnloaded()
+        {
+            AssociatedObject.MouseDoubleClick -= OnDoubleClick;
+        }
+
+        private void OnDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var obj = (DependencyObject)e.OriginalSource;
+
+            while (obj != null && !AssociatedObject.Equals(obj))
+            {
+                if (obj.GetType() == typeof(ListViewItem))
+                {
+                    var context = ((ListViewItem) obj).DataContext;
+                    if (Command != null && Command.CanExecute(context))
+                    {
+                        Command.Execute(context);
+                    }
+
+                    break;
+                }
+                obj = VisualTreeHelper.GetParent(obj);
+            }
+        }
+    }
+}
