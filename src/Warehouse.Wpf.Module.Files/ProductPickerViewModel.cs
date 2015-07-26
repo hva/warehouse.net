c0cffced1c92a255f38a22b1920532593d05019e
Yauheni Khvaliuk <hvaluk@gmail.com>
2015-07-26 15:49:04 +0200

    desktop - files - product picker - search

--- src/Warehouse.Wpf.Module.Files/ProductPickerViewModel.cs
+++ src/Warehouse.Wpf.Module.Files/ProductPickerViewModel.cs
@@ -1,11 +1,15 @@
 ﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Windows.Data;
 using System.Windows.Input;
 using Microsoft.Practices.Prism.Commands;
 using Microsoft.Practices.Prism.Interactivity.InteractionRequest;
 using Microsoft.Practices.Prism.Mvvm;
 using Warehouse.Wpf.Data.Interfaces;
+using Warehouse.Wpf.Infrastructure;
 using Warehouse.Wpf.Models;
 
 namespace Warehouse.Wpf.Module.Files
@@ -13,7 +17,9 @@ namespace Warehouse.Wpf.Module.Files
     public class ProductPickerViewModel : BindableBase, IConfirmation, IInteractionRequestAware
     {
         private readonly IProductsRepository productsRepository;
-        private ProductName[] items;
+        private string searchQuery;
+        private readonly CollectionViewSource cvs;
+        private readonly ObservableCollection<ProductName> items;
 
         public ProductPickerViewModel(IProductsRepository productsRepository)
         {
@@ -21,17 +27,29 @@ namespace Warehouse.Wpf.Module.Files
 
             CancelCommand = new DelegateCommand(Close);
             PickCommand = new DelegateCommand(Pick);
+
+            items = new ObservableCollection<ProductName>();
+            cvs = new CollectionViewSource { Source = items };
+            cvs.SortDescriptions.Add(new SortDescription("Name", ListSortDirection.Ascending));
         }
 
         public ICommand CancelCommand { get; private set; }
         public ICommand PickCommand { get; private set; }
         public object[] SelectedItems { get; set; }
         public ProductName[] SelectedProducts { get; private set; }
+        public ICollectionView Items { get { return cvs.View; } }
 
-        public ProductName[] Items
+        public string SearchQuery
         {
-            get { return items; }
-            set { SetProperty(ref items, value); }
+            get { return searchQuery; }
+            set
+            {
+                if (searchQuery != value)
+                {
+                    searchQuery = value;
+                    cvs.View.Refresh();
+                }
+            }
         }
 
         #region IInteractionRequestAware
@@ -50,7 +68,25 @@ namespace Warehouse.Wpf.Module.Files
             var task = await productsRepository.GetNamesAsync();
             if (task.Succeed)
             {
-                Items = task.Result.ToArray();
+                items.Clear();
+                items.AddRange(task.Result);
+
+                cvs.Filter -= OnFilter;
+                cvs.Filter += OnFilter;
+            }
+        }
+
+        private void OnFilter(object sender, FilterEventArgs e)
+        {
+            if (string.IsNullOrEmpty(searchQuery))
+            {
+                return;
+            }
+
+            var item = e.Item as ProductName;
+            if (item != null)
+            {
+                e.Accepted = item.Name.IndexOf(searchQuery, StringComparison.InvariantCultureIgnoreCase) >= 0;
             }
         }
 
