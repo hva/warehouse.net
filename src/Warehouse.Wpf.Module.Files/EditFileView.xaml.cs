0a769fc93bdfb8db4dc72da10bd64cfe8d60c978
Yauheni Khvaliuk <hvaluk@gmail.com>
2015-07-28 10:48:10 +0200

    desktop - files - edit product - interaction request trigger fixed

--- src/Warehouse.Wpf.Module.Files/EditFileView.xaml.cs
+++ src/Warehouse.Wpf.Module.Files/EditFileView.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Interactivity;
 
 namespace Warehouse.Wpf.Module.Files
 {
@@ -7,6 +9,23 @@ namespace Warehouse.Wpf.Module.Files
         public EditFileView()
         {
             InitializeComponent();
+
+            Loaded += OnLoaded;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            // InteractionRequestTrigger works only once for windows created by another InteractionRequestTrigger
+            // Hot fix: attaching trigger manually
+            // TODO: check why it happens
+            var context = DataContext as CreateFileViewModel;
+            if (context != null)
+            {
+                var triggers = Interaction.GetTriggers(this);
+                triggers.Clear();
+                trigger.SourceObject = context.AddProductRequest;
+                triggers.Add(trigger);
+            }
         }
     }
 }
