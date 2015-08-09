f49d49239db89812deacb24c41d4fcc27229e65b
Yauheni Khvaliuk <hvaluk@gmail.com>
2015-08-09 12:13:15 +0200

    files - zoom image - popup window size fixed

--- src/Warehouse.Wpf.Module.Files/FileView.xaml.cs
+++ src/Warehouse.Wpf.Module.Files/FileView.xaml.cs
@@ -15,6 +15,26 @@ namespace Warehouse.Wpf.Module.Files
 
         private void OnLoaded(object sender, RoutedEventArgs e)
         {
+            FixTrigger();
+            FixWindowSize();
+        }
+
+        private void FixWindowSize()
+        {
+            // By default, PopupWindowAction creates window with SizeToContent = SizeToContent.WidthAndHeight
+            // It changes window size when zooming
+            // Hot fix: set window size manually
+            var window = Parent as Window;
+            if (window != null)
+            {
+                window.SizeToContent = SizeToContent.Manual;
+                window.Width = 400;
+                window.Height = 600;
+            }
+        }
+
+        private void FixTrigger()
+        {
             // InteractionRequestTrigger works only once for windows created by another InteractionRequestTrigger
             // Hot fix: attaching trigger manually
             // TODO: check why it happens
