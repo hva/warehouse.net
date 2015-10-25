066f0f79fa4d692b1493cdd36dc9f83aceb17b94
Yauheni Khvaliuk <hvaluk@gmail.com>
2015-10-25 22:14:22 +0100

    edit dialog started

---
+++ src/Warehouse.Wpf/Modules/OperationsModule/OperationEditViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Practices.Prism.Interactivity.InteractionRequest;
+
+namespace Warehouse.Wpf.Modules.OperationsModule
+{
+    public class OperationEditViewModel : INotification
+    {
+        public OperationEditViewModel()
+        {
+            Title = "Новая накладная";
+        }
+
+        public string Title { get; set; }
+        public object Content { get; set; }
+    }
+}
--- src/Warehouse.Wpf/Modules/OperationsModule/OperationsListViewModel.cs
+++ src/Warehouse.Wpf/Modules/OperationsModule/OperationsListViewModel.cs
@@ -1,20 +1,34 @@
-﻿using Microsoft.Practices.Prism.Mvvm;
+﻿using System.Windows.Input;
+using Microsoft.Practices.Prism.Commands;
+using Microsoft.Practices.Prism.Interactivity.InteractionRequest;
+using Microsoft.Practices.Prism.Mvvm;
 
 namespace Warehouse.Wpf.Modules.OperationsModule
 {
     public class OperationsListViewModel : BindableBase
     {
         private bool isBusy;
+        private readonly InteractionRequest<OperationEditViewModel> createRequest;
 
         public OperationsListViewModel()
         {
-            
+            CreateCommand = new DelegateCommand(Create);
+            createRequest = new InteractionRequest<OperationEditViewModel>();
         }
 
         public bool IsBusy
         {
             get { return isBusy; }
             set { SetProperty(ref isBusy, value); }
+        }
+
+        public ICommand CreateCommand { get; private set; }
+
+        public IInteractionRequest CreateRequest => createRequest;
+
+        private void Create()
+        {
+            createRequest.Raise(new OperationEditViewModel());
         }
     }
 }
